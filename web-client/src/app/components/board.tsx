e5e805129bd50fd38d9a0c9e0efd77b6e11cf49f
BradyBassett <bradydbassett@gmail.com>
2023-12-02 20:38:38 -0600

    started fen parsing

--- web-client/src/app/components/board.tsx
+++ web-client/src/app/components/board.tsx
@@ -2,24 +2,16 @@
 
 import React, { useState } from "react";
 import styles from '../../styles/board.module.scss';
-import Bishop from "./pieces/bishop";
-import King from "./pieces/king";
-import Knight from "./pieces/knight";
-import Pawn from "./pieces/pawn";
-import Queen from "./pieces/queen";
-import Rook from "./pieces/rook";
+import Piece from "./piece";
+import { parseFenPosition } from "../util/fenParser";
 
 
-import variables from "../../styles/_variables.module.scss";
-
-
-interface Square {
-  piece: string | null;
-}
+type Square = string | null;
 
 export default function Board(): React.ReactElement {
   const [board, setBoard] = useState(Array(8).fill(Array(8).fill(null)));
-  const squareSize = parseFloat(variables.squareSize)
+  const fenPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // w KQkq - 0 1
+  const startingPositions = parseFenPosition(fenPosition); 
 
   return (
     <div className={styles.boardContainer}>
@@ -30,8 +22,7 @@ export default function Board(): React.ReactElement {
               <div 
                 key={colIndex} 
                 className={`${styles.square} ${(rowIndex + colIndex) % 2 === 0 ? styles.light : styles.dark}`}>
-                  {/* todo - insert pieces into position */}
-                  <Queen size={squareSize} color="light"/>
+
               </div>
             ))}
           </div>
---
+++ web-client/src/app/util/fenParser.tsx
@@ -0,0 +1,50 @@
+import Piece from "../components/piece";
+import variables from "../../styles/_variables.module.scss";
+
+
+type Pieces = (React.ReactElement | 0)[]
+type PieceMapping = {
+  [key: string]: ["rook" | "knight" | "bishop" | "queen" | "king" | "pawn", "dark" | "light"];
+};
+
+export function parseFenPosition(fenPosition: string): Pieces {
+  const pieces: Pieces = [];
+  const squareSize = parseFloat(variables.squareSize);
+  const pieceMappings: PieceMapping = {
+    r: ["rook", "dark"],
+    n: ["knight", "dark"],
+    b: ["bishop", "dark"],
+    q: ["queen", "dark"],
+    k: ["king", "dark"],
+    p: ["pawn", "dark"],
+    R: ["rook", "light"],
+    N: ["knight", "light"],
+    B: ["bishop", "light"],
+    Q: ["queen", "light"],
+    K: ["king", "light"],
+    P: ["pawn", "light"]
+  }
+
+  for (const char of fenPosition.split("")) {
+    let color: string;
+    let type: string;
+
+    if (char == "/"){
+      continue;
+    }
+    else if (isDigitFrom1To8(char)){
+      for (let i = 0; i < parseInt(char); i++) {
+        pieces.push(0);
+      }
+    } 
+    else {      // All remaining possible characters represent pieces
+      const [type, color] = pieceMappings[char];
+      pieces.push(<Piece size={squareSize} type={type} color={color}/>)
+    }
+  }
+  return pieces;
+}
+
+function isDigitFrom1To8(char: string): boolean {
+  return /^[1-8]$/.test(char);
+}
