b9a203f601f7b12c6f50a0d0fb437463ace53c88
BradyBassett <bradydbassett@gmail.com>
2023-12-02 21:13:50 -0600

    mapping starting positions to board

--- web-client/src/app/components/board.tsx
+++ web-client/src/app/components/board.tsx
@@ -4,14 +4,22 @@ import React, { useState } from "react";
 import styles from '../../styles/board.module.scss';
 import Piece from "./piece";
 import { parseFenPosition } from "../util/fenParser";
+import PieceProps from "@/interfaces/PieceProps";
 
 
-type Square = string | null;
+type Square = React.ReactElement<PieceProps> | null;
 
 export default function Board(): React.ReactElement {
-  const [board, setBoard] = useState(Array(8).fill(Array(8).fill(null)));
-  const fenPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // w KQkq - 0 1
-  const startingPositions = parseFenPosition(fenPosition); 
+  const fenCode = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // w KQkq - 0 1
+  const startingPositions = parseFenPosition(fenCode);
+  
+  const [board, setBoard] = useState(() => {
+    const boardArray: Square[][] = [];
+    for (let i = 0; i < startingPositions.length; i += 8) {
+      boardArray.push(startingPositions.slice(i, i + 8));
+    }
+    return boardArray;
+  });
 
   return (
     <div className={styles.boardContainer}>
@@ -22,7 +30,7 @@ export default function Board(): React.ReactElement {
               <div 
                 key={colIndex} 
                 className={`${styles.square} ${(rowIndex + colIndex) % 2 === 0 ? styles.light : styles.dark}`}>
-
+                  {square}
               </div>
             ))}
           </div>
