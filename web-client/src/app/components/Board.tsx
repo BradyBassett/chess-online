1dd4b4d6de3ff37531b0cc6afe193164fcd6a154
BradyBassett <bradydbassett@gmail.com>
2023-12-08 22:30:09 -0600

    active squares and basic piece moving

--- web-client/src/app/components/Board.tsx
+++ web-client/src/app/components/Board.tsx
@@ -11,19 +11,26 @@ import styles from '../../styles/board.module.scss';
 export type PossiblePiece = React.ReactElement<PieceProps> | null;
 
 export default function Board(): React.ReactElement {
-  const fenCode = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // w KQkq - 0 1
-  const startingPositions = parseFenPosition(fenCode);
-  
-  const [board, setBoard] = useState(() => {
+  const [board, setBoard] = useState(setStartingPositions());
+
+  const [selectedSquare, setSelectedSquare] = useState<number[]>([]);
+  const [destinationSquare, setDestinationSquare] = useState<number[]>([]);
+
+  function resetBoard() {
+    setBoard(setStartingPositions());
+  }
+
+  function setStartingPositions() {
+    const fenCode = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" // w KQkq - 0 1
+    const startingPositions = parseFenPosition(fenCode);
+
     const boardArray: PossiblePiece[][] = [];
     for (let i = 0; i < startingPositions.length; i += 8) {
       boardArray.push(startingPositions.slice(i, i + 8));
     }
-    return boardArray;
-  });
 
-  const [selectedSquare, setSelectedSquare] = useState<number[]>([]);
-  const [destinationSquare, setDestinationSquare] = useState<number[]>([]);
+    return boardArray;
+  }
 
   function handleSquareClick(rowIndex: number, colIndex: number) {
     // if selectedSquare has not been set, set selected square
@@ -33,41 +40,53 @@ export default function Board(): React.ReactElement {
     // If selectedSquare has been set, set new square as destination square
     else {
       setDestinationSquare([rowIndex, colIndex])
+      const selectedPiece = board[selectedSquare[0]][selectedSquare[1]];
 
-      updateSquare(rowIndex, colIndex, board[selectedSquare[0]][selectedSquare[1]])
+      // only update the square if selectedSquare is not null
+      if (selectedPiece !== null) {
+        updateSquare(selectedSquare[0], selectedSquare[1], null);
+        updateSquare(rowIndex, colIndex, selectedPiece);
+      }
 
       setSelectedSquare([]);
       setDestinationSquare([]);
     }
   }
 
-  function updateSquare(rowIndex: number, colIndex: number, newPiece: PossiblePiece) {
+  function updateSquare(rowIndex: number, colIndex: number, piece: PossiblePiece) {
     setBoard(prevBoard => {
       const newBoard = [...prevBoard];
       newBoard[rowIndex] = [...prevBoard[rowIndex]];
-      newBoard[rowIndex][colIndex] = newPiece;
+      newBoard[rowIndex][colIndex] = piece;
 
       return newBoard;
     })
   }
 
   return (
-    <div className={styles.boardContainer}>
-      <div className={styles.board}>
-        {board.map((row, rowIndex) => (
-          <div key={rowIndex} className={styles.boardRow}>
-            {row.map((piece: PossiblePiece, colIndex: number) => (
-              <Square 
-                key={colIndex} 
-                color={(rowIndex + colIndex) % 2 === 0 ? "light" : "dark"}
-                onClick={() => handleSquareClick(rowIndex, colIndex)}
-              >
-                {piece}
-              </Square>
-            ))}
-          </div>
-        ))}
+    <>
+      <button onClick={() => resetBoard()}>Reset Board</button>
+      <div className={styles.boardContainer}>
+        <div className={styles.board}>
+          {board.map((row, rowIndex) => (
+            <div key={rowIndex} className={styles.boardRow}>
+              {row.map((piece: PossiblePiece, colIndex: number) => (
+                <Square 
+                  key={colIndex} 
+                  color={(rowIndex + colIndex) % 2 === 0 ? "light" : "dark"}
+                  isActive={
+                    selectedSquare[0] === rowIndex &&
+                    selectedSquare[1] === colIndex
+                  }
+                  onClick={() => handleSquareClick(rowIndex, colIndex)}
+                >
+                  {piece}
+                </Square>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- web-client/src/app/components/Square.tsx
+++ web-client/src/app/components/Square.tsx
@@ -7,21 +7,15 @@ import styles from '../../styles/square.module.scss';
 interface SquareProps {
   color: "light" | "dark";
   children: PossiblePiece;
+  isActive: boolean;
   onClick: () => void;
 }
 
-export default function Square({color, children, onClick}: SquareProps): React.ReactElement {
-  const [selected, setSelected] = useState(false);
-
-  function handleClick() {
-    onClick();
-    setSelected(!selected);
-  }
-
+export default function Square({color, children, isActive, onClick}: SquareProps): React.ReactElement {
   return (
     <div 
-      className={`${styles.square} ${color === "light" ? styles.light : styles.dark} ${selected ? styles.activeSquare : ""}`}
-      onClick={handleClick}
+      className={`${styles.square} ${color === "light" ? styles.light : styles.dark} ${isActive ? styles.activeSquare : ""}`}
+      onClick={onClick}
     >
       {children}
     </div>
