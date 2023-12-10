b16c2898d08f99def7f45e781ecbc422a2813e47
BradyBassett <bradydbassett@gmail.com>
2023-12-09 23:30:20 -0600

    added tests for Piece component

--- web-client/src/app/components/Piece.test.tsx
+++ web-client/src/app/components/Piece.test.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Piece from "../components/Piece";
+import '@testing-library/jest-dom';
+import Piece, { PieceProps } from "../components/Piece";
 
-describe("Pieces component", () => {
+
+describe("Piece component", () => {
+  describe("Piece component variations", () => {
+    const pieces = ["bishop", "king", "knight", "pawn", "queen", "rook"];
   
+    pieces.forEach((pieceType) => {
+      it(`renders Piece component with dark ${pieceType} correctly`, () => {
+        render(<Piece size={50} color="dark" type={pieceType as PieceProps["type"]} />);
+  
+        expect(screen.getByAltText(`Dark${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
+        expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/dark-${pieceType}.svg`);
+      });
+  
+      it(`renders Piece component with light ${pieceType} correctly`, () => {
+        render(<Piece size={50} color="light" type={pieceType as PieceProps["type"]} />);
+  
+        expect(screen.getByAltText(`Light${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
+        expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/light-${pieceType}.svg`);
+      });
+    });
+  });
 });
