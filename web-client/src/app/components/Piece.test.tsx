141ee265b28a3e9f4720274d60df35170e7df4a5
BradyBassett <bradydbassett@gmail.com>
2023-12-09 23:44:27 -0600

    removed duplicated describe

--- web-client/src/app/components/Piece.test.tsx
+++ web-client/src/app/components/Piece.test.tsx
@@ -5,23 +5,21 @@ import Piece, { PieceProps } from "../components/Piece";
 
 
 describe("Piece component", () => {
-  describe("Piece component variations", () => {
-    const pieces = ["bishop", "king", "knight", "pawn", "queen", "rook"];
-  
-    pieces.forEach((pieceType) => {
-      it(`renders Piece component with dark ${pieceType} correctly`, () => {
-        render(<Piece size={50} color="dark" type={pieceType as PieceProps["type"]} />);
-  
-        expect(screen.getByAltText(`Dark${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
-        expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/dark-${pieceType}.svg`);
-      });
-  
-      it(`renders Piece component with light ${pieceType} correctly`, () => {
-        render(<Piece size={50} color="light" type={pieceType as PieceProps["type"]} />);
-  
-        expect(screen.getByAltText(`Light${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
-        expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/light-${pieceType}.svg`);
-      });
+  const pieces = ["bishop", "king", "knight", "pawn", "queen", "rook"];
+
+  pieces.forEach((pieceType) => {
+    it(`renders Piece component with dark ${pieceType} correctly`, () => {
+      render(<Piece size={50} color="dark" type={pieceType as PieceProps["type"]} />);
+
+      expect(screen.getByAltText(`Dark${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
+      expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/dark-${pieceType}.svg`);
+    });
+
+    it(`renders Piece component with light ${pieceType} correctly`, () => {
+      render(<Piece size={50} color="light" type={pieceType as PieceProps["type"]} />);
+
+      expect(screen.getByAltText(`Light${pieceType.charAt(0).toUpperCase() + pieceType.slice(1)}`)).toBeInTheDocument();
+      expect(screen.getByRole("img")).toHaveAttribute("src", `/images/Pieces/light-${pieceType}.svg`);
     });
   });
 });
