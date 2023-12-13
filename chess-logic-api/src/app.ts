34fc380bcb5faf63bb36c2752fc2f6dcfe5b7ee6
BradyBassett <bradydbassett@gmail.com>
2023-12-12 22:08:22 -0600

    basic validate move api

--- chess-logic-api/src/app.ts
+++ chess-logic-api/src/app.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express from "express";
 const app = express();
-const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.post("/validate-move", (req, res) => {
+  const { piece, from, to } = req.body;
+
+  res.json(
+    { 
+      status: "success",
+      message: `Validating move for ${piece} from ${from} to ${to}`,
+    }
+  );
 });
 
+const port = process.env.port || 3001;
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
