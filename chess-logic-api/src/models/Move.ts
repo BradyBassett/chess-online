76c5f5e7242e6003781a1ccab311e5de4899fcb3
BradyBassett <bradydbassett@gmail.com>
2023-12-13 20:36:51 -0600

    added constructor

--- chess-logic-api/src/models/Move.ts
+++ chess-logic-api/src/models/Move.ts
@@ -1,3 +1,10 @@
+import Player from "./Player";
+import Square from "./Square";
+
 export default class Move {
-  
+  constructor(public player: Player, public from: Square, public to: Square) {
+    this.player = player;
+    this.from = from;
+    this.to = to;
+  }
 }
