f41c2f55ce8bdbd5caa0e404bfa1199871d489fa
BradyBassett <bradydbassett@gmail.com>
2023-12-14 00:23:56 -0600

    implemented isValidMove logic for bishop

--- chess-logic-api/src/models/Bishop.ts
+++ chess-logic-api/src/models/Bishop.ts
@@ -16,6 +16,28 @@ export default class Bishop extends Piece {
     if (!super.isValidMove(targetSquare, board)) {
       return false;
     }
+
+    // if the targetSquare is not a diagonal of currentSquare, return false
+    if (Math.abs(targetSquare.rowIndex - this.currentSquare.rowIndex) !== Math.abs(targetSquare.colIndex - this.currentSquare.colIndex)) {
+      return false;
+    }
+
+    // if there are pieces between the valid path between the current square and the target square, return false
+    const rowStep = (targetSquare.rowIndex - this.currentSquare.rowIndex) > 0 ? 1 : -1;
+    const colStep = (targetSquare.colIndex - this.currentSquare.colIndex) > 0 ? 1 : -1;
+    
+    let i = this.currentSquare.rowIndex + rowStep
+    let j = this.currentSquare.colIndex + colStep
+    while ((rowStep > 0 ? i < targetSquare.rowIndex : i > targetSquare.rowIndex) && 
+           (colStep > 0 ? j < targetSquare.colIndex : j > targetSquare.colIndex)) {
+        
+      if (board.getSquare(i, j).piece) {
+        return false;
+      }
+            
+      i += rowStep;
+      j += colStep;
+    }
     
     return true;
   }
