3836be47c4948b654339d95fd3a95dcddda79c29
BradyBassett <bradydbassett@gmail.com>
2023-12-13 20:36:21 -0600

    seperated setStartingPosition into function

--- chess-logic-api/src/models/Board.ts
+++ chess-logic-api/src/models/Board.ts
@@ -4,6 +4,11 @@ import Square from './Square';
 export default class Board {
   public squares: Square[][] = [];
   constructor(fenPosition: string) {
+    this.setStartingPosition(fenPosition);
+  }
+
+  public setStartingPosition(fenPosition: string): void {
+    this.squares = [];
     const parsedSquares = parseFenPosition(fenPosition);
     for (let i = 0; i < parsedSquares.length; i += 8) {
       this.squares.push(parsedSquares.slice(i, i + 8));
