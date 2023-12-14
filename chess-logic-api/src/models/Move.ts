fc18ab8d3039447730198416f7801bba1cb68bdb
BradyBassett <bradydbassett@gmail.com>
2023-12-13 22:51:03 -0600

    added move type variable to move class

--- chess-logic-api/src/models/Move.ts
+++ chess-logic-api/src/models/Move.ts
@@ -1,10 +1,12 @@
 import Player from "./Player";
 import Square from "./Square";
+import { MoveType } from "../Utilities/enums";
 
 export default class Move {
-  constructor(public player: Player, public from: Square, public to: Square) {
+  constructor(public player: Player, public from: Square, public to: Square, public type: MoveType) {
     this.player = player;
     this.from = from;
     this.to = to;
+    this.type = type;
   }
 }
