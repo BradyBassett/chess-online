016c8f7cff88709ffb95520f759a8e5d3db3184e
BradyBassett <bradydbassett@gmail.com>
2023-12-14 17:11:30 -0600

    implemented knight move logic

--- chess-logic-api/src/models/Knight.ts
+++ chess-logic-api/src/models/Knight.ts
@@ -16,7 +16,26 @@ export default class Knight extends Piece {
     if (!super.isValidMove(targetSquare, board)) {
       return false;
     }
+
+    const moves = [
+    { row: 2, col: 1 }, // up 2 right 1
+    { row: 2, col: -1 }, // up 2 left 1
+    { row: -2, col: 1 }, // down 2 right 1
+    { row: -2, col: -1 }, // down 2 left 1
+    { row: 1, col: 2 }, // up 1 right 2
+    { row: 1, col: -2 }, // up 1 left 2
+    { row: -1, col: 2 }, // down 1 right 2
+    { row: -1, col: -2 }, // down 1 left 2
+  ];
+
+  // Check if the target square matches any of the possible moves
+  for (const move of moves) {
+    if (targetSquare.rowIndex === this.currentSquare.rowIndex + move.row &&
+        targetSquare.colIndex === this.currentSquare.colIndex + move.col) {
+      return true;
+    }
+  }
     
-    return true;
+    return false;
   }
 }
