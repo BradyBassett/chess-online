5592e93895e371058440df5ea60c50fb316ca1ba
BradyBassett <bradydbassett@gmail.com>
2023-12-15 20:36:08 -0600

    refactored to only include middleware declaration

--- chess-logic-api/src/app.ts
+++ chess-logic-api/src/app.ts
@@ -1,20 +1,14 @@
 import express from "express";
+import cors from 'cors';
+import { boardRouter } from "./routes/boardRouter";
+
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+app.use("/api/board", boardRouter);
 
-app.post("/validate-move", (req, res) => {
-  const { piece, from, to } = req.body;
-
-  res.json(
-    { 
-      status: "success",
-      message: `Validating move for ${piece} from ${from} to ${to}`,
-    }
-  );
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
 
-const port = process.env.port || 3001;
-app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
-});
+export default app;
