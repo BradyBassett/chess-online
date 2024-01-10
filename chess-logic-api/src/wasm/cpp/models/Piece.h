65dac0c9ef6ca86ecba8e87d17efe3a37e632f31
BradyBassett <bradydbassett@gmail.com>
2024-01-09 23:13:32 -0600

    implemented piece class

--- chess-logic-api/src/wasm/cpp/models/Piece.h
+++ chess-logic-api/src/wasm/cpp/models/Piece.h
@@ -11,24 +11,30 @@ class Piece {
 protected:
 	bool hasMoved = false;
 	bool isCaptured = false;
-	PieceColor color;
+	PieceColor pieceColor;
 	PieceType pieceType;
-	Square* currentSquare;
+	Square& currentSquare;
 
-	bool isValidDiagonalMove(Board* board, Square* targetSquare) const;
+	bool isValidDiagonalMove(Board& board, Square& targetSquare) const;
 
-	bool targetSquareIsDiagonal(Square* targetSquare) const;
+	bool targetSquareIsDiagonal(Square& targetSquare) const;
 
-	bool isValidStraightMove(Board* board, Square* targetSquare) const;
+	bool isValidStraightMove(Board& board, Square& targetSquare) const;
 
-	bool targetSquareIsStraight(Square* targetSquare) const;
+	bool targetSquareIsStraight(Square& targetSquare) const;
 
 public:
+	Piece(PieceColor pieceColor, Square& currentSquare);
+
+	bool isValidMove(Board& board, Square& targetSquare) const;
+	
+	virtual bool getHasMoved() const = 0;
+
+	virtual bool getIsCaptured() const = 0;
+
+	virtual PieceColor getPieceColor() const = 0;
+
 	virtual PieceType getPieceType() const = 0;
-
-	Piece(PieceColor color, Square* currentSquare);
-
-	bool isValidMove(Board* board, Square* targetSquare) const;
 };
 
 #endif
