11d51e89694e5894a55ecb58d430961b540aff03
BradyBassett <bradydbassett@gmail.com>
2024-01-09 23:13:17 -0600

    removed unecessary this->

--- chess-logic-api/src/wasm/cpp/models/Board.cpp
+++ chess-logic-api/src/wasm/cpp/models/Board.cpp
@@ -2,7 +2,7 @@
 #include "../utils/fenParser.h"
 
 Board::Board(std::string fenPosition) {
-	this->setStartingPosition(fenPosition);
+	setStartingPosition(fenPosition);
 }
 
 void Board::setStartingPosition(std::string fenPosition) {
@@ -11,12 +11,12 @@ void Board::setStartingPosition(std::string fenPosition) {
 	std::vector<Square> parsedSquares = parseFenPosition(fenPosition); // TODO: implement parseFenPosition
 
 	for (int i = 0; i < parsedSquares.size(); i += 8) {
-		this->squares.push_back(std::vector<Square>(parsedSquares.begin() + i, parsedSquares.begin() + i + 8));
+		squares.push_back(std::vector<Square>(parsedSquares.begin() + i, parsedSquares.begin() + i + 8));
 	}
 }
 
 Square Board::getSquare(int rowIndex, int colIndex) {
-	return this->squares[rowIndex][colIndex];
+	return squares[rowIndex][colIndex];
 }
 
 // TODO : imoplement toJson method
