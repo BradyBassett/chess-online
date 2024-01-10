1fb586243356aadb61f9746477d74fb90734edfc
BradyBassett <bradydbassett@gmail.com>
2024-01-09 23:12:59 -0600

    moved members to private and added getters

--- chess-logic-api/src/wasm/cpp/models/Square.cpp
+++ chess-logic-api/src/wasm/cpp/models/Square.cpp
@@ -7,3 +7,15 @@ Square::Square(int rowIndex, int colIndex, std::shared_ptr<Piece> piece)
 Square::Square(const Square& square)
   : rowIndex(square.rowIndex), colIndex(square.colIndex), piece(std::move(square.piece)) {
 }
+
+u_int8_t Square::getRowIndex() const {
+  return rowIndex;
+}
+
+u_int8_t Square::getColIndex() const {
+  return colIndex;
+}
+
+std::shared_ptr<Piece> Square::getPiece() const {
+  return piece;
+}
--- chess-logic-api/src/wasm/cpp/models/Square.h
+++ chess-logic-api/src/wasm/cpp/models/Square.h
@@ -5,14 +5,21 @@
 #include "Piece.h"
 
 class Square {
-public:
+private:
 	uint8_t rowIndex;
 	uint8_t colIndex;
 	std::shared_ptr<Piece> piece;
 
+public:
 	Square(int rowIndex, int colIndex, std::shared_ptr<Piece> piece = nullptr);
 
 	Square(const Square& square);
+
+	uint8_t getRowIndex() const;
+
+	uint8_t getColIndex() const;
+
+	std::shared_ptr<Piece> getPiece() const;
 };
 
 #endif
