39127f2e8f35f2cc7245cfcdcd5dfc4f1c794f81
BradyBassett <bradydbassett@gmail.com>
2024-01-10 22:05:38 -0600

    Implemented basic move validation for knight

---
+++ chess-logic-api/src/wasm/cpp/models/Knight.cpp
@@ -0,0 +1,32 @@
+#include "Knight.h"
+#include "../utils/structs/Move.h"
+
+Knight::Knight(PieceColor pieceColor, Square& currentSquare) : Piece(pieceColor, currentSquare) {
+  pieceType = PieceType::Knight;
+}
+
+bool Knight::isValidMove(Board& board, Square& targetSquare) {
+  if (!Piece::isValidMove(board, targetSquare)) {
+    return false;
+  }
+
+  Move moves[] = {
+    {2, 1}, // up 2 right 1
+    {2, -1}, // up 2 left 1
+    {-2, 1}, // down 2 right 1
+    {-2, -1}, // down 2 left 1
+    {1, 2}, // up 1 right 2
+    {1, -2}, // up 1 left 2
+    {-1, 2}, // down 1 right 2
+    {-1, -2}, // down 1 left 2
+  };
+
+  for (const auto& move : moves) {
+    if (targetSquare.getRowIndex() == currentSquare.getRowIndex() + move.row &&
+        targetSquare.getColIndex() == currentSquare.getColIndex() + move.col) {
+      return true;
+    }
+  }
+
+  return false;
+}
--- chess-logic-api/src/wasm/cpp/models/Knight.h
+++ chess-logic-api/src/wasm/cpp/models/Knight.h
@@ -1,6 +1,13 @@
 #ifndef KNIGHT_H
 #define KNIGHT_H
 
+#include "Piece.h"
 
+class Knight : virtual public Piece {
+  public:
+  Knight(PieceColor pieceColor, Square& currentSquare);
+
+  bool isValidMove(Board& board, Square& targetSquare);
+};
 
 #endif
