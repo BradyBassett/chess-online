f4a2c8347ae8ae9abb340d830ad2c730d990fc50
BradyBassett <bradydbassett@gmail.com>
2024-01-15 19:23:49 -0600

    made methods const

--- chess-logic-api/src/wasm/cpp/models/Pawn.cpp
+++ chess-logic-api/src/wasm/cpp/models/Pawn.cpp
@@ -1,13 +1,13 @@
 #include "Pawn.h"
 #include "Square.h"
 
-bool Pawn::moveOneSquare(Board& board, Square& targetSquare) {
+bool Pawn::moveOneSquare(Board& board, Square& targetSquare) const {
   uint8_t moveDirection = pieceColor == PieceColor::Light ? 1 : -1;
 
   return targetSquare.getRowIndex() == currentSquare.getRowIndex() + moveDirection;
 }
 
-bool Pawn::moveTwoSquares(Board& board, Square& targetSquare) {
+bool Pawn::moveTwoSquares(Board& board, Square& targetSquare) const {
   uint8_t moveDirection = pieceColor == PieceColor::Light ? 2 : -2;
 
   return targetSquare.getRowIndex() == currentSquare.getRowIndex() + moveDirection;
@@ -17,7 +17,7 @@ Pawn::Pawn(PieceColor pieceColor, Square& currentSquare) : Piece(pieceColor, cur
   pieceType = PieceType::Pawn;
 }
 
-bool Pawn::isValidMove(Board& board, Square& targetSquare) {
+bool Pawn::isValidMove(Board& board, Square& targetSquare) const {
   if (!Piece::isValidMove(board, targetSquare)) {
     return false;
   }
