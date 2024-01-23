ac17960bcc4a8ebe5ab3df98c1467bbee4c4f492
BradyBassett <bradydbassett@gmail.com>
2024-01-23 17:57:15 -0600

    starting fen code not needed as a const

--- web-client/src/app/utilities/constants.ts
+++ web-client/src/app/utilities/constants.ts
@@ -1,5 +1,4 @@
 import variables from "../../styles/_variables.module.scss";
 
 
-export const STARTING_POSITION_FEN_CODE = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 export const SQUARE_SIZE = parseFloat(variables.squareSize);
