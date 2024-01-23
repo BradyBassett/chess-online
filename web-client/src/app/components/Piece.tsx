8c772c6bbdc64fcba7d20dec2142e544d11058b7
BradyBassett <bradydbassett@gmail.com>
2024-01-23 17:57:36 -0600

    reworked types to work with chess.js

--- web-client/src/app/components/Piece.tsx
+++ web-client/src/app/components/Piece.tsx
@@ -1,36 +1,42 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/piece.module.scss";
+import { PieceSymbol, Color } from "chess.js";
 
-
-export type PieceTypes = "bishop" | "king" | "knight" | "pawn" | "queen" | "rook";
-export type PieceColors = "dark" | "light";
 
 export interface PieceProps {
   size: number;
-  color: PieceColors;
-  type: "bishop" | "king" | "knight" | "pawn" | "queen" | "rook";
+  color: Color;
+  type: PieceSymbol;
 }
 
 export default function Piece({ size, color, type }: PieceProps): React.ReactElement {
-  function getIconSource() {
-    const imagePrefix = color === "dark" ? "dark-" : "light-";
-    const imageSrc = `/images/Pieces/${imagePrefix}${type}.svg`;
-    return imageSrc;
-  };
-
   if (Number.isNaN(size) || size === undefined || size === null || size === 0) {
     size = 45;
   }
+
+  const colorMap = {
+    "b": "Black",
+    "w": "White"
+  };
+
+  const typeMap = {
+    "b": "Bishop",
+    "k": "King",
+    "n": "Knight",
+    "p": "Pawn",
+    "q": "Queen",
+    "r": "Rook"
+  };
 
   return (
     <Image 
       className={styles.piece}
       data-testid="piece"
-      src={getIconSource()}
+      src={`/images/Pieces/${color}-${type}.svg`}
       width={size}
       height={size}
-      alt={color.charAt(0).toUpperCase() + color.slice(1) + type.charAt(0).toUpperCase() + type.slice(1)} // takes first char in color and type and converts to uppercase
+      alt={`${colorMap[color]} ${typeMap[type]}`}
     />
   );
 };
