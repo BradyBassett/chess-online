bc525090af05c3bc882e6b3a11586044504de479
BradyBassett <bradydbassett@gmail.com>
2024-02-01 16:52:05 -0600

    Fixed White Knight Key

--- chess-engine-api/Chess/utils/fenParser.cpp
+++ chess-engine-api/Chess/utils/fenParser.cpp
@@ -22,7 +22,7 @@ std::vector<Square> parseFenPosition(std::string& fenPosition) {
 		{'k', [](Square square) {return std::make_shared<King>(Color::Black, square); }},
 		{'p', [](Square square) {return std::make_shared<Pawn>(Color::Black, square); }},
 		{'R', [](Square square) {return std::make_shared<Rook>(Color::White, square); }},
-		{'K', [](Square square) {return std::make_shared<Knight>(Color::White, square); }},
+		{'N', [](Square square) {return std::make_shared<Knight>(Color::White, square); }},
 		{'B', [](Square square) {return std::make_shared<Bishop>(Color::White, square); }},
 		{'Q', [](Square square) {return std::make_shared<Queen>(Color::White, square); }},
 		{'K', [](Square square) {return std::make_shared<King>(Color::White, square); }},
@@ -30,6 +30,7 @@ std::vector<Square> parseFenPosition(std::string& fenPosition) {
 	};
 
 	for (char c : fenPosition) {
+		// If character is a /, start the next row, ensuring numSquaresPerRow is equal to 8
 		if (c == '/') {
 			if (rowIndex != 8 || colIndex >= 7) {
 				throw std::invalid_argument("Invalid Fen Position");
@@ -37,15 +38,18 @@ std::vector<Square> parseFenPosition(std::string& fenPosition) {
 			rowIndex = 0;
 			colIndex++;
 		}
+		// if the character is any digit from 1-8 add empty squares to pieces (represented as null)
 		else if (isDigitFrom1To8(c)) {
 			for (uint8_t i = 0; i < c - '0'; i++) {
 				squares.push_back(Square(rowIndex, colIndex));
 				rowIndex++;
 			}
 		}
+		// If the character is not "/", a digit from 1-8, or a key in pieceMappings throw an error
 		else if (pieceTypes.count(c) == 0) {
 			throw std::invalid_argument("Invalid Fen Position");
 		}
+		// All remaining possible characters should represent pieces
 		else {
 			Square square = Square(rowIndex, colIndex);
 			std::shared_ptr<Piece> piece = pieceTypes[c](square);
