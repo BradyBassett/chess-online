64546d4744cbcf97f33adbc5dfa04f8c94519380
BradyBassett <bradydbassett@gmail.com>
2024-01-31 19:35:23 -0600

    added basic game logic

--- chess-engine-api/Chess/main.cpp
+++ chess-engine-api/Chess/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "models/Game.h"
 
 int main();
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 
+	Game game = Game();
+	std::cout << game.ascii() << std::endl;
+
 	return 0;
 }
+
+// find . -name "*.cpp" | xargs g++ -o main
