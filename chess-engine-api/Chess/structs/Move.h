e8ff2c430c1666f1903601a955fd24337591fe5c
BradyBassett <bradydbassett@gmail.com>
2024-02-02 17:25:28 -0600

    Delete Move class and Move.h, add new Move struct

---
+++ chess-engine-api/Chess/structs/Move.h
@@ -0,0 +1,32 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include "Position.h"
+#include "../enums/PieceType.h"
+#include "../enums/Color.h"
+#include "../enums/MoveFlag.h"
+#include <string>
+#include <bitset>
+#include <optional>
+
+struct Move {
+	std::bitset<7> flags; // NonCapture, PawnPush, EnPassant, StandardCapture, Promotion, KingsideCastling, QueensideCastling
+
+	void setFlag(MoveFlag flag) {
+		flags.set(flag);
+	}
+
+	bool hasFlag(MoveFlag flag) {
+		return flags.test(flag);
+	}
+
+	Color color;
+	Position from;
+	Position to;
+	std::string flags;
+	PieceType piece;
+	std::optional<PieceType> capturedPiece;
+	std::optional<PieceType> promotion;
+};
+
+#endif
