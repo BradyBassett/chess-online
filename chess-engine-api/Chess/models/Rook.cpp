acb1064b2f5af7ce194379ca30f51db45fd0f0c0
BradyBassett <bradydbassett@gmail.com>
2024-02-04 18:19:01 -0600

    Add side property to Rook class

--- chess-engine-api/Chess/models/Rook.cpp
+++ chess-engine-api/Chess/models/Rook.cpp
@@ -2,4 +2,14 @@
 
 Rook::Rook(Color pieceColor, Position currentPosition) : Piece(pieceColor, currentPosition), StraightPiece(pieceColor, currentPosition) {
 	pieceType = PieceType::Rook;
+
+	if (currentPosition.col == 0) {
+		side = Side::QueenSide;
+	} else {
+		side = Side::KingSide;
+	}
+}
+
+Side Rook::getSide() const {
+	return side;
 }
--- chess-engine-api/Chess/models/Rook.h
+++ chess-engine-api/Chess/models/Rook.h
@@ -4,8 +4,13 @@
 #include "StraightPiece.h"
 
 class Rook : public StraightPiece {
+private:
+	Side side;
+
 public:
 	Rook(Color pieceColor, Position currentPosition);
+
+	Side getSide() const;
 };
 
 #endif
