6f8a6add7945d9182f1677c02493908a70e3404d
BradyBassett <bradydbassett@gmail.com>
2024-02-27 16:18:14 -0600

    Added validation for En Passant

--- chess-engine-api/Chess/models/Pawn.cpp
+++ chess-engine-api/Chess/models/Pawn.cpp
@@ -16,16 +16,16 @@ Pawn::Pawn(Color pieceColor, Position currentPosition) : Piece(pieceColor, curre
 	pieceType = PieceType::Pawn;
 }
 
-bool Pawn::isValidMove(Board &board, Position targetPosition, std::string &errorMessage) const
+bool Pawn::isValidMove(Game &game, Position targetPosition, std::string &errorMessage) const
 {
-	if (!Piece::isValidMove(board, targetPosition, errorMessage))
+	if (!Piece::isValidMove(game, targetPosition, errorMessage))
 	{
 		return false;
 	}
 
 	if (hasMoved)
 	{
-		if (!canMoveOneSquare(board, targetPosition))
+		if (!canMoveOneSquare(game.getBoard(), targetPosition))
 		{
 			errorMessage = "Invalid move - Pawn can only move one square forward after its first move";
 			return false;
@@ -33,17 +33,32 @@ bool Pawn::isValidMove(Board &board, Position targetPosition, std::string &error
 	}
 	else
 	{
-		if (!canMoveOneSquare(board, targetPosition) && !canMoveTwoSquares(board, targetPosition))
+		if (!canMoveOneSquare(game.getBoard(), targetPosition) && !canMoveTwoSquares(game.getBoard(), targetPosition))
 		{
 			errorMessage = "Invalid move - Pawn can only move one or two squares forward on its first move";
 			return false;
 		}
 	}
 
+	// if piece is on en passant eligible row
+	// if last move was a pawn move of 2 squares that landed next to this pawn
+	// then this pawn can capture the other pawn en passant
+
+	int enPassantEligibleRow = pieceColor == Color::White ? 3 : 4;
+	Move lastMove = game.getLastMove();
+
+	if (currentPosition.row == enPassantEligibleRow)
+	{
+		if (!(lastMove.hasFlag(MoveFlag::PawnPush) && (lastMove.to.col == currentPosition.col + 1 || lastMove.to.col == currentPosition.col - 1)))
+		{
+			errorMessage = "Invalid move - Pawn can only capture en passant if the opponents move was a pawn move of 2 squares that landed next to this pawn";
+			return false;
+		}
+	}
 	return true;
 }
 
-Bitboard Pawn::getValidMoves(Board &board) const
+Bitboard Pawn::getValidMoves(Game &game) const
 {
 	Bitboard validMoves = 0x0;
 
@@ -53,7 +68,7 @@ Bitboard Pawn::getValidMoves(Board &board) const
 		if (targetPosition.row >= 0 && targetPosition.row < 8 && targetPosition.col >= 0 && targetPosition.col < 8)
 		{
 			std::string errorMessage;
-			if (isValidMove(board, targetPosition, errorMessage))
+			if (isValidMove(game, targetPosition, errorMessage))
 			{
 				validMoves.setBit(targetPosition);
 			}
@@ -68,7 +83,7 @@ Bitboard Pawn::getValidMoves(Board &board) const
 			if (targetPosition.row >= 0 && targetPosition.row < 8 && targetPosition.col >= 0 && targetPosition.col < 8)
 			{
 				std::string errorMessage;
-				if (isValidMove(board, targetPosition, errorMessage))
+				if (isValidMove(game, targetPosition, errorMessage))
 				{
 					validMoves.setBit(targetPosition);
 				}
