6674cd0ecc0f5001adcb711fd241571c149328af
BradyBassett <bradydbassett@gmail.com>
2024-03-08 18:11:24 -0600

    Add equality and inequality operators to Position struct

--- chess-engine-api/Chess/structs/Position.h
+++ chess-engine-api/Chess/structs/Position.h
@@ -5,6 +5,16 @@ struct Position
 {
 	int row;
 	int col;
+
+	bool operator==(const Position &other) const
+	{
+		return row == other.row && col == other.col;
+	}
+
+	bool operator!=(const Position &other) const
+	{
+		return !(*this == other);
+	}
 };
 
 #endif // POSITION_H
