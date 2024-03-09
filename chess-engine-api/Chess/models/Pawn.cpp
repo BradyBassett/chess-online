e91992a447b3f5e3ba155bcac4df5a16df86d6b2
BradyBassett <bradydbassett@gmail.com>
2024-03-08 18:12:10 -0600

    Reworked pawn validation logic

--- chess-engine-api/Chess/models/Pawn.cpp
+++ chess-engine-api/Chess/models/Pawn.cpp
@@ -8,6 +8,14 @@ bool Pawn::canMoveOneSquare(Board &board, Position targetPosition) const
 
 bool Pawn::canMoveTwoSquares(Board &board, Position targetPosition) const
 {
+	int stepDirection = pieceColor == Color::White ? 1 : -1;
+	std::shared_ptr<Piece> piece = board.getSquare(targetPosition.row + stepDirection, targetPosition.col).getPiece();
+
+	if (piece != nullptr)
+	{
+		return false;
+	}
+
 	return currentPosition.row == targetPosition.row + doubleStepDirection;
 }
 
@@ -16,44 +24,60 @@ Pawn::Pawn(Color pieceColor, Position currentPosition) : Piece(pieceColor, curre
 	pieceType = PieceType::Pawn;
 }
 
-bool Pawn::isValidMove(Game &game, Position targetPosition, std::string &errorMessage) const
+bool Pawn::isValidMove(Board &board, Position targetPosition, std::string &errorMessage) const
 {
-	if (!Piece::isValidMove(game, targetPosition, errorMessage))
+	if (!Piece::isValidMove(board, targetPosition, errorMessage))
 	{
 		return false;
 	}
 
-	if (hasMoved)
+	// Diagonal move
+	if (abs(targetPosition.col - currentPosition.col) == 1 && abs(targetPosition.row - currentPosition.row) == 1)
 	{
-		if (!canMoveOneSquare(game.getBoard(), targetPosition))
+		std::shared_ptr<Piece> targetPiece = board.getSquare(targetPosition).getPiece();
+		if (targetPiece == nullptr)
 		{
-			errorMessage = "Invalid move - Pawn can only move one square forward after its first move";
-			return false;
+			int enPassantEligibleRow = pieceColor == Color::White ? 3 : 4;
+			if (currentPosition.row == enPassantEligibleRow)
+			{
+				Square *enPassantTargetSquare = board.getEnPassantTargetSquare();
+				if (enPassantTargetSquare == nullptr || targetPosition != board.getEnPassantTargetSquare()->getPosition())
+				{
+					errorMessage = "Invalid move - Pawn can only capture en passant if the opponents move was a pawn move of 2 squares that landed next to this pawn";
+					return false;
+				}
+			}
+			else
+			{
+				errorMessage = "Invalid move - Pawn cannot move diagonally unless it is capturing an opponent's piece";
+				return false;
+			}
+		}
+		else
+		{
+			if (targetPiece->getPieceColor() == pieceColor)
+			{
+				errorMessage = "Invalid move - Pawn can only capture pieces of the opposite color";
+				return false;
+			}
 		}
 	}
+	// Straight move
 	else
 	{
-		if (!canMoveOneSquare(game.getBoard(), targetPosition) && !canMoveTwoSquares(game.getBoard(), targetPosition))
+		if (hasMoved)
 		{
-			errorMessage = "Invalid move - Pawn can only move one or two squares forward on its first move";
-			return false;
-		}
-	}
-
-	// if there has been at least one move
-	if (game.getMoves().size() > 0)
-	{
-		// if piece is on en passant eligible row
-		// if last move was a pawn move of 2 squares that landed next to this pawn
-		// then this pawn can capture the other pawn en passant
-		int enPassantEligibleRow = pieceColor == Color::White ? 3 : 4;
-		Move lastMove = game.getLastMove();
-
-		if (currentPosition.row == enPassantEligibleRow)
-		{
-			if (!(lastMove.hasFlag(MoveFlag::PawnPush) && (lastMove.to.col == currentPosition.col + 1 || lastMove.to.col == currentPosition.col - 1)))
+			if (!canMoveOneSquare(board, targetPosition))
 			{
-				errorMessage = "Invalid move - Pawn can only capture en passant if the opponents move was a pawn move of 2 squares that landed next to this pawn";
+				errorMessage = "Invalid move - Pawn can only move one square forward after its first move";
+				return false;
+			}
+		}
+		else
+		{
+			if (!canMoveOneSquare(board, targetPosition) && !canMoveTwoSquares(board, targetPosition))
+			{
+				errorMessage = "Invalid move - Pawn can only move two squares forward if the square in front of it is empty and it is its first move";
 				return false;
 			}
 		}
@@ -62,7 +86,7 @@ bool Pawn::isValidMove(Game &game, Position targetPosition, std::string &errorMe
 	return true;
 }
 
-Bitboard Pawn::getValidMoves(Game &game) const
+Bitboard Pawn::getValidMoves(Board &game) const
 {
 	Bitboard validMoves = 0x0;
 
