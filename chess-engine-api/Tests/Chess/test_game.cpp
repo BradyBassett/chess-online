1a89ac68ba24f430849e8c4cfcfa8846e6ed3b3f
BradyBassett <bradydbassett@gmail.com>
2024-04-05 18:20:26 -0500

    split into 2 tests added time tracking

--- chess-engine-api/Tests/Chess/test_game.cpp
+++ chess-engine-api/Tests/Chess/test_game.cpp
@@ -1,10 +1,27 @@
 #include <gtest/gtest.h>
+#include <chrono>
 #include "../../Chess/models/Game.h"
 
-TEST(PerftTest, TestInitialPosition)
+TEST(PerftTest, TestInitialPosition_Depth1)
 {
 	Game game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+	// Perft(1) = 20
+	auto start = std::chrono::high_resolution_clock::now();
 	ASSERT_EQ(game.Perft(1), 20);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << "Perft(1) took " << duration.count() << " milliseconds.\n";
+}
+
+TEST(PerftTest, TestInitialPosition_Depth2)
+{
+	Game game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+	// Perft(2) = 400
+	auto start = std::chrono::high_resolution_clock::now();
 	ASSERT_EQ(game.Perft(2), 400);
-	// Add more depths as needed...
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << "Perft(2) took " << duration.count() << " milliseconds.\n";
 }
