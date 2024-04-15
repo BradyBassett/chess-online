0341c1837f8383ef557e26ed214ecaec48780e10
BradyBassett <bradydbassett@gmail.com>
2024-04-14 20:06:59 -0500

    init

---
+++ chess-engine-api/Tests/Chess/Bitboard/BitboardTest.hpp
@@ -0,0 +1,17 @@
+#ifndef BITBOARDTEST_HPP
+#define BITBOARDTEST_HPP
+
+#include "../ChessTest.hpp"
+#include "../../../Chess/models/Bitboard.hpp"
+
+class BitboardTest : public ChessTest
+{
+private:
+
+protected:
+
+public:
+
+};
+
+#endif // BITBOARDTEST_HPP
