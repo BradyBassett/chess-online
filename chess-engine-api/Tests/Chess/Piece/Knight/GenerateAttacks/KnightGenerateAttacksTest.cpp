9d4866b381f007bc800d202e0e6df84e0afe603d
BradyBassett <bradydbassett@gmail.com>
2024-04-16 15:15:02 -0500

    formatting

--- chess-engine-api/Tests/Chess/Piece/Knight/GenerateAttacks/KnightGenerateAttacksTest.cpp
+++ chess-engine-api/Tests/Chess/Piece/Knight/GenerateAttacks/KnightGenerateAttacksTest.cpp
@@ -2,28 +2,28 @@
 
 KnightGenerateAttacksTest::KnightGenerateAttacksTest() : param(GetParam().second) {}
 
-TEST_P(KnightGenerateAttacksTest, generateAttacks) {
-    Knight knight(param.color, param.currentPosition);
-    Bitboard actualAttacks = knight.generateAttacks();
-    Bitboard expectedAttacks = Bitboard(param.expectedAttacks);
+TEST_P(KnightGenerateAttacksTest, generateAttacks)
+{
+	Knight knight(param.color, param.currentPosition);
+	Bitboard actualAttacks = knight.generateAttacks();
+	Bitboard expectedAttacks = Bitboard(param.expectedAttacks);
 
-    ASSERT_EQ(actualAttacks, expectedAttacks);
+	ASSERT_EQ(actualAttacks, expectedAttacks);
 }
 
 std::vector<std::pair<std::string, PieceGenerateAttacksTestParam>> KnightGenerateAttacksTest::testCases = {
-    {"Center_Black", PieceGenerateAttacksTestParam{{3, 3}, Color::Black, 0x142200221400}},
-    {"Center_White", PieceGenerateAttacksTestParam{{4, 4}, Color::White, 0x28440044280000}},
-    {"Corner_Black", PieceGenerateAttacksTestParam{{0, 0}, Color::Black, 0x20400}},
-    {"Corner_White", PieceGenerateAttacksTestParam{{7, 7}, Color::White, 0x20400000000000}},
-    {"Edge_Black", PieceGenerateAttacksTestParam{{0, 3}, Color::White, 0x142200}},
-    {"Edge_White", PieceGenerateAttacksTestParam{{3, 0}, Color::White, 0x20400040200}}
-};
+	{"Center_Black", PieceGenerateAttacksTestParam{{3, 3}, Color::Black, 0x142200221400}},
+	{"Center_White", PieceGenerateAttacksTestParam{{4, 4}, Color::White, 0x28440044280000}},
+	{"Corner_Black", PieceGenerateAttacksTestParam{{0, 0}, Color::Black, 0x20400}},
+	{"Corner_White", PieceGenerateAttacksTestParam{{7, 7}, Color::White, 0x20400000000000}},
+	{"Edge_Black", PieceGenerateAttacksTestParam{{0, 3}, Color::White, 0x142200}},
+	{"Edge_White", PieceGenerateAttacksTestParam{{3, 0}, Color::White, 0x20400040200}}};
 
 INSTANTIATE_TEST_SUITE_P(
-    KnightGenerateAttacks,
-    KnightGenerateAttacksTest,
-    ::testing::ValuesIn(KnightGenerateAttacksTest::testCases),
-	[](const testing::TestParamInfo<KnightGenerateAttacksTest::ParamType>& info) {
+	KnightGenerateAttacks,
+	KnightGenerateAttacksTest,
+	::testing::ValuesIn(KnightGenerateAttacksTest::testCases),
+	[](const testing::TestParamInfo<KnightGenerateAttacksTest::ParamType> &info)
+	{
 		return info.param.first;
-	}
-);
+	});
