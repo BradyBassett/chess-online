b0ca2c613ec225747a6aa4587c140cea32529da1
BradyBassett <bradydbassett@gmail.com>
2024-04-18 01:08:57 -0500

    Refactor Tests class to include a new method for generating random uint64 values with optional min and max parameters

--- chess-engine-api/Tests/Tests.cpp
+++ chess-engine-api/Tests/Tests.cpp
@@ -2,11 +2,12 @@
 #include <random>
 #include <iostream>
 
-uint64_t Tests::generateRandomUint64Value() {
+uint64_t Tests::generateRandomUint64Value(uint64_t min = std::numeric_limits<uint64_t>::min(), uint64_t max = std::numeric_limits<uint64_t>::max()) {
     std::random_device rd;
     std::mt19937_64 generator(rd());
 
-    uint64_t value = generator();
+    std::uniform_int_distribution<uint64_t> distribution(min, max);
+    uint64_t value = distribution(generator);
 
-	return value;
+    return value;
 }
--- chess-engine-api/Tests/Tests.hpp
+++ chess-engine-api/Tests/Tests.hpp
@@ -2,6 +2,7 @@
 #define TESTS_HPP
 
 #include <gtest/gtest.h>
+#include <limits>
 
 class Tests
 {
@@ -10,7 +11,7 @@ private:
 protected:
 
 public:
-	static uint64_t generateRandomUint64Value();
+	static uint64_t generateRandomUint64Value(uint64_t min = std::numeric_limits<uint64_t>::min(), uint64_t max = std::numeric_limits<uint64_t>::max());
 };
 
 #endif // TESTS_HPP
