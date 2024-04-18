6941d8b30b7771527c2243bedbe4655af7509774
BradyBassett <bradydbassett@gmail.com>
2024-04-18 18:05:12 -0500

    removed default values from definition

--- chess-engine-api/Tests/Tests.cpp
+++ chess-engine-api/Tests/Tests.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <iostream>
 
-uint64_t Tests::generateRandomUint64Value(uint64_t min = std::numeric_limits<uint64_t>::min(), uint64_t max = std::numeric_limits<uint64_t>::max()) {
+uint64_t Tests::generateRandomUint64Value(uint64_t min, uint64_t max) {
     std::random_device rd;
     std::mt19937_64 generator(rd());
 
